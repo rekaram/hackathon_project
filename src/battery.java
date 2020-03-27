e40e00a5b0d52ee6d0c0efa9c9f80f9aa8061d83
rekaram <51747472+rekaram@users.noreply.github.com>
2020-03-27 15:16:50 +0530

    Update and rename battery_file to battery.java

--- src/battery.java
+++ src/battery.java
@@ -11,29 +11,34 @@ public class Battery {
 		String compare1="Uid u0a202";
 		String compare2="Foreground activities";
 		float Battery_percentage=0,Battery_drain=0;
+		//import file path to read text file
 		File file = new File("C:\\Users\\user\\Desktop\\project\\Battery.txt"); 
 		BufferedReader br = new BufferedReader(new FileReader(file)); 
 		String st,st2,st3,Foreground=""; 
+		//reaing the text file line by line
 		while ((st = br.readLine()) != null) 
 		{
 			st2=st;
+			//use ":" as a delimiter to split the line into array
 			String[] array = st2.split(":");
 			if(array.length>1)
 			{ 
+			        //removing space at the front and back of the string
 				array[0]=array[0].trim();
+				//String comparision 
 				if(array[0].equals(compare1)  )
 				{
-		         st3=array[1];
-		         String[] array1 = st3.split("\\(");
-		         Battery_drain=Float.parseFloat(array1[0]); 
-		         //System.out.println(Battery_drain);
+		         		st3=array[1];
+		         		String[] array1 = st3.split("\\(");
+		         		Battery_drain=Float.parseFloat(array1[0]); 
+		         		//System.out.println(Battery_drain);
 				}
 				if(array[0].equals(compare2))
 				{
-                 st3=array[1];
-		         String[] array1 = st3.split("\\(r");
-		         Foreground=array1[0].trim();
-		         //System.out.println(Foreground);
+                		 	st3=array[1];
+		         		String[] array1 = st3.split("\\(r");
+		         		Foreground=array1[0].trim();
+		         		//System.out.println(Foreground);
 				} 
 			}
 	   
@@ -46,6 +51,7 @@ public class Battery {
 		obj.put("Foreground_time",Foreground);
 		obj.put("Battery_drain" ,Battery_drain);
 		obj.put("Battery_percentage" ,Battery_percentage);
+		//creating json file to push the output
 		FileWriter file1=new FileWriter("C:\\Users\\user\\Desktop\\project\\output.json");
 		file1.write(obj.toString());
 		file1.flush();
